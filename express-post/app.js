63c523d3ed0ee9b649fc9b69e29c02d9c64635f9
Jan Redmann <janredmann1@gmail.com>
2021-02-08 11:48:06 +0100

    add boilerplate

--- express-post/app.js
+++ express-post/app.js
@@ -1,6 +1,32 @@
 const express = require('express');
 const app = express();
 
+// this line is needed to parse the request body
+app.use(express.urlencoded({ extended: false }));
+
+let accessCount = 0;
+// middlewares
+const count = () => {
+    return (req, res, next) => {
+        accessCount++
+        console.log(accessCount);
+        next();
+    }
+}
+
+// registering a middleware globally -> for every route
+// app.use(count());
+
+// registering middleware (count) for this route
+app.get('/', count(), (req, res) => {
+    res.render('form');
+})
+
+app.post('/post-example', (req, res) => {
+    // we use req.body to retrieve the values from the input fields 
+    console.log('this is the username: ', req.body.user);
+})
+
 app.set('view engine', 'hbs');
 
 app.listen(3000);
